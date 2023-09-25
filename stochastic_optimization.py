9abfd092b8d059cbb741f3e8172a1988d1174350
Reid Passmore <tpassmore6@gatech.edu>
2023-09-25 09:49:46 -0700

    added flow chart

--- stochastic_optimization.py
+++ stochastic_optimization.py
@@ -24,12 +24,43 @@ Pre-Algo:
 - set parameter search range
 
 Algo Steps:
-Using initial beta
+
+Todo:
+Using initial betas to see how well they recreate routes
 
 
 '''
 
 fp = Path.home() / "Documents/GitHub/Impedance-Calibration"
+
+#import dict of matched traces with link ids and geometry 
+with (fp/'matched_traces.pkl').open('rb') as fh:
+     matched_traces = pickle.load(fh)
+
+#import trips to attach data to
+trips_df = pd.read_csv(fp/'trips.csv')
+
+#reduce tripsdf size
+trips_df = trips_df[trips_df['tripid'].isin(set(matched_traces.keys()))]
+
+#select 50 random ones
+
+
+#retrieve the ods for shortest path routing 
+ods = { key : (matched_traces[key]['nodes'][0],matched_traces[key]['nodes'][-1]) for key, item in matched_traces.items()}
+
+#add start and end node to trips_df
+trips_df['od'] = trips_df['tripid'].map(ods)
+
+#add GPS distance
+
+#add match distance
+#ovtrips_df['']
+
+ods = set(ods)
+sources = set([x[0] for x in ods])
+
+#%%
 
 #import network links
 links = gpd.read_file(fp/"final_network.gpkg",layer='links')
@@ -37,31 +68,21 @@ links['length_ft'] = links.length
 links['cost'] = links.length
 links['tup'] = list(zip(links['A'],links['B']))
 
-cols = ['length_ft','osm_mu']
-alpha = 1
-
 #make graph
 G = nx.DiGraph()  # create directed graph
-for ind, row in links.iterrows():
-    G.add_weighted_edges_from([(str(row['A']), str(row['B']), float(row['cost']))],weight='weight')   
+for ind, row in links[['A','B','cost']].itertuples(index=False):
+    G.add_weighted_edges_from([(row[0], row[1], float(row[2]))],weight='weight')   
 
 #pre-formatting of variables
 
 #create dict of link lengths for lookup
 link_lengths = dict(zip(links['tup'],links['length_ft']))
 
-#import dict of matched traces with link ids and geometry 
-with (fp/'matched_traces.pkl').open('rb') as fh:
-     matched_traces = pickle.load(fh)
+#create dict of link geos for lookup
+link_geos = dict(zip(links['tup'],links['geometry']))
 
+#%% do shortest path routing with distance based impedance
 
-#retrieve the ods for shortest path routing 
-ods = set([(matched_traces[key]['nodes'][0],matched_traces[key]['nodes'][-1]) for key, item in matched_traces.items()])
-sources = set([x[0] for x in ods])
-
-#%%
-
-#do shortest path routing
 shortest_paths_dist = {}
 print('Shortest path routing')
 for source in tqdm(sources):
@@ -80,11 +101,16 @@ for source in tqdm(sources):
 with (fp/'shortest_paths.pkl').open('wb') as fh:
      pickle.dump(shortest_paths_dist,fh)
 
+#%% load dict
+with (fp/'shortest_paths.pkl').open('rb') as fh:
+     shortest_paths_dist = pickle.load(fh)
+
+#%% attach shortest distance to trip df
+
+trips_df['shortest_distance'] = trips_df['od'].map(shortest_paths_dist)
+
 
 #%%
-
-
-
 
 #reduce size of matched traces for testing
 random = np.random.choice(np.asarray(list(matched_traces.keys())),size=50,replace=False)
@@ -96,8 +122,6 @@ sum_all = np.asarray([matched_traces[key]['matched_trip'].length.sum() for key i
 
 #define parameter search range for each variable
 bounds = [[0,2],[-2,2]]
-
-
 
 
 #link cost function
@@ -208,5 +232,41 @@ x = minimize(objective_function, bounds, args=(links,sources,G), method='pso')
 end = time.time()
 print(f'Took {(end-start)/60/60} hours')
 
-#%%
+#%% test params
 
+betas = np.array([0.03,-3.89])
+
+links['cost'] = links.apply(lambda link: link_cost(link,betas),axis=1)
+
+#update edge weights
+nx.set_edge_attributes(G,values=weights,name='weight')
+
+#do shortest path routing
+shortest_paths = {}
+print(f'Shortest path routing with coefficients: {betas}')
+for source in tqdm(sources):
+    #perform shortest path routing (from one to all)
+    length, paths = nx.single_source_dijkstra(G,source,weight='weight')
+    #retrieve target nodes
+    targets = [x[1] for x in ods if x[0] == source]
+    for target in targets:
+        #convert from node list to edge list
+        node_list = paths[target]
+        edge_list = [ (node_list[i],node_list[i+1]) for i in range(len(node_list)-1)]
+        #add to dict
+        shortest_paths[(source,target)] = edge_list
+
+#calculate overlap
+all_overlap = 0
+for key, item in matched_traces.items():
+    source = item['nodes'][0]
+    target = item['nodes'][-1]    
+    edges = item['edges']
+    modeled_edges = shortest_paths[(source,target)]
+    list_of_lists = [edges,modeled_edges]
+    overlap = list(set.intersection(*[set(list) for list in list_of_lists]))
+    overlap = [link_lengths.get(id,0) for id in overlap]
+    all_overlap += np.asarray(overlap).sum()
+
+#calculate objective function value
+val = -1 * all_overlap / sum_all
